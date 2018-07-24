e91044ebab1a77836cf61193e9e6c42b6163ca54
pvn <pedro.vicente@space-research.org>
2018-07-24 14:42:47 -0400

    HTTP client

--- ftams_client.cs
+++ ftams_client.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
-using System.IO;
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////
 //FTAMS: File Transfer Access and Management Services
+//usage
+//http://127.0.0.1/FTAMSDeviceService/DeviceService.svc/Reports/GetMessages/3
+//http://127.0.0.1/FTAMSDeviceService/DeviceService.svc/Reports/PostMessage
+/////////////////////////////////////////////////////////////////////////////////////////////////////
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 //tcp_client_t
@@ -22,9 +25,9 @@ class tcp_client_t : socket_t
   //open
   //create a socket and call connect()
   /////////////////////////////////////////////////////////////////////////////////////////////////////
-  public int open()
+  public int open(string ip, int port)
   {
-    IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 4000);
+    IPEndPoint ipep = new IPEndPoint(IPAddress.Parse(ip), port);
     m_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
     try
     {
@@ -59,25 +62,78 @@ class ftams_client
   /////////////////////////////////////////////////////////////////////////////////////////////////////
   //Main
   /////////////////////////////////////////////////////////////////////////////////////////////////////
+
   static void Main(string[] args)
   {
-    tcp_client_t client = new tcp_client_t();
-    if (client.open() < 0)
+    const string ftams_place = "/FTAMSDeviceService/DeviceService.svc/Reports";
+    string uri = args[0];
+
+    if (uri.Contains(ftams_place) == false)
     {
       return;
     }
 
-    string http_request;
-    http_request = "test";
+    //parse URI 
+    int start = uri.IndexOf("://");
+    if (start == -1)
+    {
 
-    if (client.write_all(http_request) < 0)
+    }
+    start += 3;
+    int end = uri.IndexOf("/", start + 1);
+    string host_name = uri.Substring(start, end - start);
+    Console.WriteLine("host: {0}", host_name);
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////
+    //make HTTP request
+    /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    string http = "";
+    string message;
+    message = "test";
+
+    //get API token
+    int ftams_place_pos = uri.IndexOf(ftams_place, 0);
+    string api_token = uri.Substring(ftams_place_pos + ftams_place.Length);
+    string get_messages_nbr;
+    if (api_token.IndexOf("/GetMessages/") != -1)
+    {
+      const string get_messages = "/GetMessages/";
+      get_messages_nbr = api_token.Substring(get_messages.Length);
+      http += "GET ";
+      http += api_token;
+      http += " HTTP/1.1\r\n";
+      http += "\r\n"; //terminate HTTP header
+    }
+    else if (api_token.CompareTo("/PostMessage") == 0)
+    {
+      http += "POST ";
+      http += api_token;
+      http += " HTTP/1.1\r\n";
+      http += "Content-Length: ";
+      int message_size = message.Length;
+      http += message_size.ToString();
+      http += "\r\n";
+      http += "Connection: close\r\n";
+      http += "\r\n"; //terminate HTTP header
+      //add message
+      http += message;
+    }
+
+    tcp_client_t client = new tcp_client_t();
+    if (client.open(host_name, 4000) < 0)
+    {
+      return;
+    }
+
+    if (client.write_all(http) < 0)
     {
       client.m_socket.Close();
       return;
     }
 
     Console.WriteLine("Sent:");
-    Console.WriteLine(http_request);
+    Console.WriteLine(http);
 
     string response = socket_t.receive_stream(client.m_socket);
     Console.WriteLine("Received:");
